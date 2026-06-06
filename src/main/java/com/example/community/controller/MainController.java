3602835e803d6241e324f85e27355e602100254c
noh <noh@nohui-MacBookPro.local>
2026-06-06 19:42:36 +0900

    feat: 루트 경로 GET 메서드 응답 처리 구현

--- src/main/java/com/example/community/controller/MainController.java
+++ src/main/java/com/example/community/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.example.community.controller;
 
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +9,12 @@ import java.util.Map;
 @RestController
 public class MainController { // 루트 경로 처리
     @GetMapping("/")
-    public ResponseEntity<Map<String, String>> index() {
-        return null;
+    public ResponseEntity<Map<String, Object>> index() { // 필터 토큰 검사 성공하여야 해당 메서드 실행
+        return ResponseEntity.ok(Map.of( // 하드 코딩, 추후 응답 객체 클래스 따로 만들어서 관리
+                "message", "authorized",
+                "data", Map.of(
+                        "redirect_url", "/posts"
+                )
+        ));
     }
 }
